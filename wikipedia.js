cbd464ea1a2ea0a54af135358e7f20baf54697c7
Laurens Panier <ozafy@borknet.org>
2023-10-03 10:41:57 +0200

    update to wiki

--- wikipedia.js
+++ wikipedia.js
@@ -1,22 +1,23 @@
 // ==UserScript==
 // @name         Wikipedia full width
-// @version      1.0
+// @version      1.1
 // @description  Makes main content full width
 // @author       Ozafy
 // @match        https://*.wikipedia.org/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    //'use strict';
+(function () {
+  //'use strict';
   var x = document.getElementsByClassName("mw-page-container");
   var i;
   for (i = 0; i < x.length; i++) {
-    x[i].style.maxWidth="none";
+    x[i].style.maxWidth = "none";
   }
-  var x = document.getElementsByClassName("mw-content-container");
+  var x = document.getElementsByClassName("mw-body");
   var i;
   for (i = 0; i < x.length; i++) {
-    x[i].style.maxWidth="none";
+    x[i].style.gridTemplate = "min-content min-content min-content 1fr / 1fr min-content";
+    x[i].style.gridTemplateAreas = "'titlebar-cx .        ' 'titlebar    columnEnd' 'toolbar     columnEnd' 'content     columnEnd'";
   }
 })();
